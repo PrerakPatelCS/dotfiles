1e9bb3c1e98899a6544124612810bbae19ae7ead
PrerakPatelCS <patelprerak121@gmail.com>
2024-05-17 13:43:59 -0400

    Init

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,17 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+abbr --add win --position anywhere '/mnt/c/Users/Prerak/'
+abbr --add code 'powershell.exe code'
+
+# set -gx PATH /home/prerak/.local/bin/ $PATH
+# Add paths to $PATH
+
+fish_add_path $HOME/.local/bin/
+fish_add_path $HOME/.cargo/bin/
+
+# Set up fzf key bindings
+fzf --fish | source
+
+zoxide init fish | source
